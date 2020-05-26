f5e29455b3ce73efc09d07c9c44a2065c0435c76
khaled gabr <khaledgabr77@gmial.com>
2020-05-26 08:02:47 +0200

    creating workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/khaled/lab_kalmanfilter/src
+# Build directory: /home/khaled/lab_kalmanfilter/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/khaled/lab_kalmanfilter/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/khaled/lab_kalmanfilter/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/khaled/lab_kalmanfilter/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/khaled/lab_kalmanfilter/src:$ROS_PACKAGE_PATH"
